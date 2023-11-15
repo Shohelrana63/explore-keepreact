83ce59c0c7ff60dc338eb4de315c986bd6450de3
Shohelrana63 <shohelrana.beg@gmail.com>
2023-11-15 15:35:58 +0600

    keepreact is cool

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./components/Banner/Banner"
+import Cards from "./components/Cards/Cards"
+import Menubar from "./components/Menubar/Menubar"
+
+function App() {
+  return (
+    <>
+     <Menubar/>
+     <Banner/>
+     <Cards/>
+    </>
+  )
+}
+
+export default App
